5681e777f67f4a02a60e5098c0cf739f8cb3c430
izzicain <izzicain@gmail.com>
2017-07-11 20:16:57 -0500

    Added first JS

---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+window.onload = function(event) {
+  console.log('Hi mom');
+  console.log(document);
+  let ul = document.createElement('ul');
+  console.log(ul);
+
+  document.getElementById('list').innerHTML = 'i like cats'
+}
